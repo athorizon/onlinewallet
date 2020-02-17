c7a47d7d164ec94218b0a9f54c7c010a3e7e86aa
ArushiBhardwaj <60979386+ArushiBhardwaj@users.noreply.github.com>
2020-02-17 16:26:33 +0530

    Test Cases

--- online-wallet/src/test/java/Test.java
+++ online-wallet/src/test/java/Test.java
@@ -1,4 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Test;
 
+import com.capgemini.onlinewallet.service.AddAmountValidation;
+import com.capgemini.onlinewallet.service.ShowBalanceValidation;
 public class Test {
+	@Test
+	void showBalanceTest()
+	{
+		ShowBalanceValidation obj=new ShowBalanceValidation();
+		assertEquals(0.0,obj.showBalance(111001));
+	}
+	@Test
+	void addAmountTest()
+	{
+		AddAmountValidation obj1=new AddAmountValidation();
+		assertEquals(0.0,obj1.addAmount(111001,0));
+	}
 
 }
